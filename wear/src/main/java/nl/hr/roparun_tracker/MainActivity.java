3c3ed4c4b5d62f7cd658a0958c56897ae9b74e5b
Wouter Bruggeman <wouterbruggeman@live.nl>
2018-09-04 13:57:07 +0000

    Add heartrate sensor support

--- wear/src/main/java/nl/hr/roparun_tracker/MainActivity.java
+++ wear/src/main/java/nl/hr/roparun_tracker/MainActivity.java
@@ -1,28 +1,51 @@
 package nl.hr.roparun_tracker;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements SensorEventListener{
 
-    private TextView mTextView;
-    private SensorManager sensorManager;
-    private Sensor sensor;
+    TextView heartrateText;
+    SensorManager mSensorManager;
+    Sensor mHeartRateSensor;
+    SensorEventListener sensorEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView hometext = (TextView) findViewById(R.id.hometext);
-        hometext.setText("Please wait...");
-        
-
-        hometext.setText();
-
         // Enables Always-on
         setAmbientEnabled();
+
+        //Create text view
+        heartrateText = findViewById(R.id.heartrateTextId);
+
+        //Create sensor managers etc.
+        mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
+        mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //Set sensor delay
+        mSensorManager.registerListener(sensorEventListener, mHeartRateSensor, mSensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE){
+            String heartrate = "" + (int) event.values[0];
+            heartrateText.setText(heartrate);
+        }else{
+            heartrateText.setText("Sensor not working...");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
