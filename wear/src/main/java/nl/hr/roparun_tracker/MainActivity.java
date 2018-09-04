135ffbefe815ad1208d844ac2e2d6a6b82bd34cf
Wouter Bruggeman <wouterbruggeman@live.nl>
2018-09-04 13:23:16 +0000

    add stuff

--- wear/src/main/java/nl/hr/roparun_tracker/MainActivity.java
+++ wear/src/main/java/nl/hr/roparun_tracker/MainActivity.java
@@ -1,5 +1,7 @@
 package nl.hr.roparun_tracker;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
@@ -7,13 +9,18 @@ import android.widget.TextView;
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
+    private SensorManager sensorManager;
+    private Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView hometext = (TextView) findViewById(R.id.hometext);
+        hometext.setText("Please wait...");
+        
 
-        mTextView = (TextView) findViewById(R.id.text);
+        hometext.setText();
 
         // Enables Always-on
         setAmbientEnabled();
